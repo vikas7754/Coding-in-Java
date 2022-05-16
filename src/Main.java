59df7ab483db995a2117e90773a8e428a3928cb5
Vikas <vikask4590@gmail.com>
2022-05-16 20:56:21 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World..!");
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter String:");
+        String s = x.nextLine();
+        System.out.println(s);
+    }
+}
